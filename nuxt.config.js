29a99d0dbe5bab759883ba7ac68ca8ac15446b53
Dylan K <50585244+dylankainth@users.noreply.github.com>
2024-11-04 20:06:49 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  generate: {
+    routes: ['/1']
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
